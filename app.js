894c90ac8201f1aece82c88114b9570352ee9a72
nine96as <ashrafsaid79@gmail.com>
2023-11-06 12:28:04 +0000

    Complete create question route

--- app.js
+++ app.js
@@ -42,4 +42,26 @@ app.get('/questions/:id', (req, res) => {
     : res.status(200).send(foundQuestion);
 });
 
+// create question route
+app.post('/questions', (req, res) => {
+  const newQuestion = req.body;
+
+  if (
+    !newQuestion.question ||
+    !newQuestion.answer_1 ||
+    !newQuestion.answer_2 ||
+    !newQuestion.answer_3 ||
+    !newQuestion.correct_answer
+  )
+    res.status(422).send({
+      error:
+        'You must provide a question, answer_1 value, answer_2 value, answer_3 value, and a correct_answer value'
+    });
+
+  newQuestion.id = questions.length + 1;
+  questions.push(newQuestion);
+
+  res.status(201).send(newQuestion);
+});
+
 module.exports = app;
