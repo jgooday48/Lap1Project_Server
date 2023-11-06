0f80ab78aff49f7a0c93f0ab3a57d59cc39caf2b
“TatyanaA” <t.alchikova@gmail.com>
2023-11-06 12:09:51 +0000

    add list of qestions ,home route

--- app.js
+++ app.js
@@ -14,6 +14,25 @@ app.use(logger)
 app.get('/', (req, res) => {
     res.status(200).send('History Quiz Website')
   })
+
   
+app.get('/questions', (req, res) => {
+   res.send(questions)
+ })  
+
+app.get('/questions/:id', (req, res) => {
+  const idx = req.params.id
+
+  const question = questions[idx - 1]
   
+  if (!question) {
+    res.status(404).json({ message: `questions with id ${idx} not found` })
+  } else {
+    res.send(question)
+  }
+  })
+  
+
+
+
  module.exports = app
