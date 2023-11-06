4fdc71c2247dae2eab19182cecc2e2ce0ede280c
nine96as <ashrafsaid79@gmail.com>
2023-11-06 11:13:10 +0000

    Tweak home route response

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const cors = require('cors')
-const questions = require('./QuestionsBritish.json')
-const logger = require('./logger')
+const express = require('express');
+const cors = require('cors');
+const questions = require('./QuestionsBritish.json');
+const logger = require('./logger');
 
-const app = express()
+const app = express();
 
 // MIDDLEWARE
-
-app.use(cors())
-app.use(express.json())
-app.use(logger)
+app.use(cors());
+app.use(express.json());
+app.use(logger);
 
 app.get('/', (req, res) => {
-    res.status(200).send('History Quiz Website')
-  })
-  
-  
- module.exports = app
+  res
+    .status(200)
+    .send(
+      `Welcome to the history quiz questions API! There are ${questions.length} available.`
+    );
+});
+
+module.exports = app;
