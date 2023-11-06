4e92d637de2966a6ed52c21eb23aef85bb863faa
nine96as <nine96@skiff.com>
2023-11-06 11:33:50 +0000

    Complete specific question route

--- app.js
+++ app.js
@@ -25,4 +25,16 @@ app.get('/questions/random', (req, res) => {
   res.status(200).send(questions[randomIndex]);
 });
 
+// specific question route
+app.get('/questions/:id', (req, res) => {
+  const { id } = req.params;
+  const foundQuestion = questions.find(
+    (question) => question.id === parseInt(id)
+  );
+
+  !foundQuestion
+    ? res.status(404).send({ error: `Question with id: ${id} not found` })
+    : res.status(200).send(foundQuestion);
+});
+
 module.exports = app;
