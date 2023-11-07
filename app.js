fac7dd8e27e693327686fe820e4d45e9df9ed66d
nine96as <ashrafsaid79@gmail.com>
2023-11-07 10:43:49 +0000

    Integrate american questions into routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const questions = require('./QuestionsBritish.json');
+const british = require('./QuestionsBritish.json');
+const american = require('./QuestionsAmerican.json');
 const logger = require('./logger');
 
 const app = express();
@@ -13,25 +14,36 @@ app.use(logger);
 // home route
 app.get('/', (req, res) => {
   res.status(200).send({
-    message: `Welcome to the history quiz questions API! There are ${questions.length} available.`
+    message: `Welcome to the history quiz questions API! There are ${british.length} british questions available, and ${american.length} american questions available.`
   });
 });
 
-// list of questions route
-app.get('/questions', (req, res) => {
-  res.send(questions);
+// list of british questions route
+app.get('/questions/british', (req, res) => {
+  res.send(british);
 });
 
-// question randomiser route
-app.get('/questions/random', (req, res) => {
-  const randomIndex = Math.floor(Math.random() * questions.length);
-  res.status(200).send(questions[randomIndex]);
+// list of american questions route
+app.get('/questions/american', (req, res) => {
+  res.send(american);
 });
 
-// specific question route
-app.get('/questions/:id', (req, res) => {
+// british question randomiser route
+app.get('/questions/british/random', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * british.length);
+  res.status(200).send(british[randomIndex]);
+});
+
+// american question randomiser route
+app.get('/questions/american/random', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * american.length);
+  res.status(200).send(american[randomIndex]);
+});
+
+// specific british question route
+app.get('/questions/british/:id', (req, res) => {
   const { id } = req.params;
-  const foundQuestion = questions.find(
+  const foundQuestion = british.find(
     (question) => question.id === parseInt(id)
   );
 
@@ -40,8 +52,20 @@ app.get('/questions/:id', (req, res) => {
     : res.status(200).send(foundQuestion);
 });
 
-// create question route
-app.post('/questions', (req, res) => {
+// specific american question route
+app.get('/questions/american/:id', (req, res) => {
+  const { id } = req.params;
+  const foundQuestion = american.find(
+    (question) => question.id === parseInt(id)
+  );
+
+  !foundQuestion
+    ? res.status(404).send({ error: `Question with id: ${id} not found` })
+    : res.status(200).send(foundQuestion);
+});
+
+// create british question route
+app.post('/questions/british', (req, res) => {
   const newQuestion = req.body;
 
   if (
@@ -56,8 +80,30 @@ app.post('/questions', (req, res) => {
         'You must provide a question, answer_1 value, answer_2 value, answer_3 value, and a correct_answer value'
     });
 
-  newQuestion.id = questions.length + 1;
-  questions.push(newQuestion);
+  newQuestion.id = british.length + 1;
+  british.push(newQuestion);
+
+  res.status(201).send(newQuestion);
+});
+
+// create american question route
+app.post('/questions/american', (req, res) => {
+  const newQuestion = req.body;
+
+  if (
+    !newQuestion.question ||
+    !newQuestion.answer_1 ||
+    !newQuestion.answer_2 ||
+    !newQuestion.answer_3 ||
+    !newQuestion.correct_answer
+  )
+    res.status(422).send({
+      error:
+        'You must provide a question, answer_1 value, answer_2 value, answer_3 value, and a correct_answer value'
+    });
+
+  newQuestion.id = american.length + 1;
+  american.push(newQuestion);
 
   res.status(201).send(newQuestion);
 });
