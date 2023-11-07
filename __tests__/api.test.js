6bedb8fd71e895bb99ff75f48993b1c4f12791a0
nine96as <nine96@skiff.com>
2023-11-07 10:44:08 +0000

    Add test cases for american routes

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -59,20 +59,31 @@ describe('api server', () => {
     });
   });
 
-  describe('GET /questions -- index', () => {
-    test('responds to GET /questions with status 200', (done) => {
-      request(api).get('/questions').expect(200, done);
+  describe('GET /questions/british -- index', () => {
+    test('responds to GET /questions/british with status 200', (done) => {
+      request(api).get('/questions/british').expect(200, done);
     });
 
-    test('GET /questions display 5 elements in the web browser', async () => {
-      const response = await request(api).get('/questions');
+    test('GET /questions/british displays 6 elements in the web browser', async () => {
+      const response = await request(api).get('/questions/british');
+      expect(response.body.length).toBe(6);
+    });
+  });
+
+  describe('GET /questions/american -- index', () => {
+    test('responds to GET /questions/american with status 200', (done) => {
+      request(api).get('/questions/american').expect(200, done);
+    });
+
+    test('GET /questions/american displays 5 elements in the web browser', async () => {
+      const response = await request(api).get('/questions/american');
       expect(response.body.length).toBe(5);
     });
   });
 
-  describe('GET /questions/:id -- show', () => {
-    test('responds to GET /questions/1 with status 200', (done) => {
-      request(api).get('/questions/1').expect(200).expect(
+  describe('GET /questions/british/:id -- show', () => {
+    test('responds to GET /questions/british/1 with status 200', (done) => {
+      request(api).get('/questions/british/1').expect(200).expect(
         {
           question:
             "Who out of the following was not one of Henry VIII's wives?",
@@ -88,14 +99,37 @@ describe('api server', () => {
 
     test('responds to a unknown question id with a 404', (done) => {
       request(api)
-        .get('/questions/42')
+        .get('/questions/british/42')
         .expect(404)
         .expect({ error: 'Question with id: 42 not found' }, done);
     });
   });
 
-  describe('POST /questions', () => {
-    test('responds to POST / with status 201', (done) => {
+  describe('GET /questions/american/:id -- show', () => {
+    test('responds to GET /questions/american/1 with status 200', (done) => {
+      request(api).get('/questions/american/1').expect(200).expect(
+        {
+          question: 'who was the first US president',
+          answer_1: 'Benjamin Franklin',
+          answer_2: 'John Adams',
+          answer_3: 'Calvin Coolidge',
+          correct_answer: 'George Washington',
+          id: 1
+        },
+        done
+      );
+    });
+
+    test('responds to a unknown question id with a 404', (done) => {
+      request(api)
+        .get('/questions/american/42')
+        .expect(404)
+        .expect({ error: 'Question with id: 42 not found' }, done);
+    });
+  });
+
+  describe('POST /questions/british', () => {
+    test('responds to POST /questions/british with status 201', (done) => {
       const testData = {
         question: 'How are you today?',
         answer_1: 'ok',
@@ -104,7 +138,42 @@ describe('api server', () => {
         correct_answer: 'amazing'
       };
       request(api)
-        .post('/questions')
+        .post('/questions/british')
+        .send(testData)
+        .set('Accept', 'application/json')
+        .expect(201)
+        .expect({ ...testData, id: 7 }, done);
+    });
+
+    test('throws a 422 if create unsuccessful', (done) => {
+      const testData = { age: 99 };
+
+      request(api)
+        .post('/questions/british')
+        .send(testData)
+        .set('Accept', 'application/json')
+        .expect(422)
+        .expect(
+          {
+            error:
+              'You must provide a question, answer_1 value, answer_2 value, answer_3 value, and a correct_answer value'
+          },
+          done
+        );
+    });
+  });
+
+  describe('POST /questions/american', () => {
+    test('responds to POST /questions/american with status 201', (done) => {
+      const testData = {
+        question: 'How are you today?',
+        answer_1: 'ok',
+        answer_2: 'not bad',
+        answer_3: 'pretty good',
+        correct_answer: 'amazing'
+      };
+      request(api)
+        .post('/questions/american')
         .send(testData)
         .set('Accept', 'application/json')
         .expect(201)
@@ -115,7 +184,7 @@ describe('api server', () => {
       const testData = { age: 99 };
 
       request(api)
-        .post('/questions')
+        .post('/questions/american')
         .send(testData)
         .set('Accept', 'application/json')
         .expect(422)
